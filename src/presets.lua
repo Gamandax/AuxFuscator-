44f06339ae85f63fb341b092efdb1e04f35d23b0
Nexus <rodgiellamas@gmail.com>
2025-10-05 15:56:20 +0800

    Update presets.lua

--- src/presets.lua
+++ src/presets.lua
@@ -28,7 +28,7 @@ return {
                 Name = "ConstantArray";
                 Settings = {
                     Treshold    = 1;
-                    StringsOnly = true;
+                    StringsOnly = false;
                 }
             },
             {
